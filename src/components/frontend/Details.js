01ffaf53d7caf8f8a9bb3e5b21d96e79f75bdb24
Olga Botez <olgabotez@macbookair.local>
2020-07-06 23:07:05 +0100

    fixed the details page with changing a text

--- src/components/frontend/Details.js
+++ src/components/frontend/Details.js
@@ -4,10 +4,25 @@ import { Link } from 'react-router-dom';
 
 export default class Details extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            welcomeMessage: 'Welcome to the details page'
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                welcomeMessage: 'Work in Progress'
+            });
+        }, 3000);
+    }
+
     render() {
         return (
             <div>
-                <h1>Welcome to the Details Page, WIP!</h1>
+                <h1>{this.state.welcomeMessage}</h1>
                 <Link to='/'>Go Back</Link>
             </div>
         );
